97cf960f6877e19a9a0b448ab1ad4b4a862858eb
Rachit tambi <95204792+Coder519@users.noreply.github.com>
2022-08-10 18:07:20 +0530

    Add files via upload

---
+++ bed.js
@@ -0,0 +1,8 @@
+function draw(){
+          image(img, 0,0,640,420);
+          fill("#F0000");
+          text("Bed",45,45);
+          noFill();
+          stroke("#F0000");
+          rect(30,30,50,50);
+}       
